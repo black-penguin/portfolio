adc7f1bec9a06884ef49b755dbc4a0d118010002
Austin Wheelwright <pyrotechnician25@gmail.com>
2017-09-07 01:51:28 -0600

    spelling error on main page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
       <div className="home">
         <h1>Austin Wheelwright</h1>
         <h2>Full Stack Web Developer</h2>
-        <h2>Thanks for checkout out my Portfolio!</h2>
+        <h2>Thanks for checking out my Portfolio!</h2>
       </div>
     );
   }
