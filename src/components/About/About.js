754ea62f0ffb196855a1dc265ad54a458a12ed7c
Austin Wheelwright <pyrotechnician25@gmail.com>
2017-10-02 12:32:18 -0600

    fixed wording on about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
   render() {
     return (
       <div  className="about">
-        <h1>I first got into programing in High School, Then I job at a company called Bluehost in 2015 where I was able to look into the hosting side of websites. After Bluehost I went to DevMountain in provo with their Web Development Program and I am excited to show you what I can do!</h1>
+        <h1>I first got into programing in High School, Then I was able to get a job at a company called Bluehost in 2015 where I was able to look into the hosting side of websites. After Bluehost I went to DevMountain in provo with their Web Development Program and I am excited to show you what I can do!</h1>
       </div>
     );
   }
